75b5759cde893f6288795e070c2ce7631c159b88
melly-mel <mcanela@onemedical.com>
2022-08-04 15:33:45 -0400

    added models

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,13 @@
+// @ts-check
 import { initSchema } from '@aws-amplify/datastore';
 import { schema } from './schema';
 
-const { Scheduling, Providers, Credentials, ProviderTypes, Genders, Insurances, ProviderInsurances, ProviderPractices, Practices, Markets } = initSchema(schema);
+
+
+const { InsurancePlans, Scheduling, Providers, Credentials, ProviderTypes, Genders, Insurances, ProviderInsurances, ProviderPractices, Practices, Markets } = initSchema(schema);
 
 export {
+  InsurancePlans,
   Scheduling,
   Providers,
   Credentials,
