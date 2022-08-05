5fa622097cdcd8e48855a5c7ff0ca63f7350f290
melly-mel <86007276+melly-mel@users.noreply.github.com>
2022-08-05 15:33:27 -0400

    Dev (#6)
    * pulled updated schema changes
    
    * small edits
    
    * updating ui and model components
    
    * add ui components
    
    * added main frame ui component
    
    * removed ts config
    
    * added db service
    
    * added market selection field
    
    * fixed broken component
    
    * added typescript
    
    * added insurance and plans accepted field along with styling changes
    
    * added models
    
    * updated git ignore
    
    * removed aws exports file
    
    * updated plans accepted field
    
    * small style changes
    
    * updated git ignore
    
    * removed console logs and added practice section
    
    * downgraded amplify react-ui package
    
    * attempting to fix package deps
    
    * attempt at fixing prod build
    
    * another attempt at fixing build

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -9,11 +9,11 @@ import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
