6770a511fa48bf24e7bafbd3d26ea1ede4ff0a28
melly-mel <mcanela@onemedical.com>
2022-08-10 12:17:23 -0400

    added boto3 aws sdk

--- amplify/backend/function/marketCentralAddressFetcher/src/index.py
+++ amplify/backend/function/marketCentralAddressFetcher/src/index.py
@@ -1,9 +1,46 @@
 import json
 import googlemaps
 import os
+import boto3
+from botocore.exceptions import ClientError
 
-print(os.environ["google_places_api_key"])
-gmaps = googlemaps.Client(key=os.environ["google_places_api_key"])
+def get_secret():
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name,
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            print("The requested secret " + secret_name + " was not found")
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            print("The request was invalid due to:", e)
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            print("The request had invalid params:", e)
+        elif e.response['Error']['Code'] == 'DecryptionFailure':
+            print("The requested secret can't be decrypted using the provided KMS key:", e)
+        elif e.response['Error']['Code'] == 'InternalServiceError':
+            print("An error occurred on service side:", e)
+    else:
+        # Secrets Manager decrypts the secret value using the associated KMS CMK
+        # Depending on whether the secret was a string or binary, only one of these fields will be populated
+        if 'SecretString' in get_secret_value_response:
+            text_secret_data = get_secret_value_response['SecretString']
+        else:
+            binary_secret_data = get_secret_value_response['SecretBinary']
+
+        # Your code goes here.
+
+secret_name = os.environ["google_places_api_key"]
+print(get_secret())
+# gmaps = googlemaps.Client(key=os.environ["google_places_api_key"])
 
 def handler(event, context):
   print('received event:')
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,13 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "function": {
+        "marketCentralAddressFetcher": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
     }
 }
